bf6a454bf13a735ef193ad2e0dea217eba9ce04d
Valeria <popescu.valeria99@gmail.com>
2024-03-14 17:30:58 +0200

    Added search all books by bookstore id in BookToBookstore

--- src/main/java/md/usm/book_store/controllers/thymeleaf/BookToBookstoreControllerThymeleaf.java
+++ src/main/java/md/usm/book_store/controllers/thymeleaf/BookToBookstoreControllerThymeleaf.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequiredArgsConstructor
 @Controller
@@ -31,6 +32,7 @@ public class BookToBookstoreControllerThymeleaf {
         }
         bookstore_name = bookstoreService.findBookstoreById(bookstore_id).getName();
         model.addAttribute("bookBookstoreDto", bookBookstoreDto);
+        model.addAttribute("bookstore_id", bookstore_id);
         model.addAttribute("bookList", bookService.findAllBooks());
         model.addAttribute("bookListForTable", bookstoreService.findBookstoreById(bookstore_id).getBookList());
         model.addAttribute("bookstoreName", bookstore_name);
@@ -49,5 +51,13 @@ public class BookToBookstoreControllerThymeleaf {
     public String deleteBookFromBookstore(@RequestParam Integer book_id){
         bookstoreService.deleteBookFromBookstore(book_id, bookstore_id);
         return "redirect:/bookToBookstore";
+    }
+
+    @GetMapping("/bookToBookstore/bookstore")
+    public ModelAndView getBooksByBookstoreId(@RequestParam(name = "bookstore_id", required = false) Integer bookstore_id){
+        ModelAndView modelAndView = new ModelAndView("redirect:/bookToBookstore");
+        modelAndView.addObject("bookstore_id", bookstore_id);
+        this.bookstore_id = bookstore_id;
+        return modelAndView;
     }
 }
--- src/main/java/md/usm/book_store/controllers/thymeleaf/UserControllerThymeleaf.java
+++ src/main/java/md/usm/book_store/controllers/thymeleaf/UserControllerThymeleaf.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Controller;
 public class UserControllerThymeleaf {
     private final UserService userService;
 
+
 }
