c8f3c91164b600392b98ef80d460b7abff5d8af0
Valeria <popescu.valeria99@gmail.com>
2024-05-23 17:12:21 +0300

    Improved project

---
+++ src/main/java/md/usm/book_store/models/dto/LoginAdmin.java
@@ -0,0 +1,15 @@
+package md.usm.book_store.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginAdmin {
+    private String username;
+    private String password;
+}
